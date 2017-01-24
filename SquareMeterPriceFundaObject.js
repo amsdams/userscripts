2386a550b05b480e436e011b8ca01b4dc93aab67
amsdams <julien.langeveld@gmail.com>
2017-01-24 17:14:08 +0100

    Update SquareMeterPriceFundaObject.js

--- SquareMeterPriceFundaObject.js
+++ SquareMeterPriceFundaObject.js
@@ -10,42 +10,75 @@
 
 (function() {
     'use strict';
+    var bDetail = document.querySelectorAll('main div.object-detail').length==1;
+    var bOverview = document.querySelectorAll('main form.search').length==1;
 
-    // Your code here...
-    var descs = document.querySelectorAll(".object-kenmerken-list > *, .object-kenmerken-group-list > dl > *");
-    var tWoonoppervlakte;
-    var tVraagprijs;
-    var nVierkanteMeterPrijs;
+    if (bOverview){
+        // Your code here...
+        var objects = document.querySelectorAll(".search-result-content-inner");
+        for (var i=0; i < objects.length; ++i){
+            var object = objects[i];
+            var eVraagprijs = object.querySelector('.search-result-price');
+            var eWoonoppervlakte = object.querySelector('[title=Woonoppervlakte]');
+            var tVraagprijs = eVraagprijs.innerText || eVraagprijs.textContent;
+            var tWoonoppervlakte = eWoonoppervlakte.innerText || eWoonoppervlakte.textContent;
 
-    for (var i=0; i < descs.length; ++i){
-        var desc = descs[i];
-        var text = desc.innerText || desc.textContent;
-        console.log(text);
+            var nVraagprijs = tVraagprijs.replace(/[^0-9]/g, "");
+            var nWoonoppervlakte = tWoonoppervlakte.replace(/[^0-9]/g, "");
 
-        if (text == 'Vraagprijs'){
-            var eVraagprijs = descs[i+1];
-            tVraagprijs = eVraagprijs.innerText || eVraagprijs.textContent;
+            var  nVierkanteMeterPrijs = nVraagprijs/nWoonoppervlakte;
+
+            var eVierkanteMeterPrijs = createEVierkanteMeterPrijs(nVierkanteMeterPrijs);
+            object.appendChild(eVierkanteMeterPrijs);
+
+
 
         }
-
-        /* Gebruiksoppervlakten*/
-        if (text  == 'Woonoppervlakte'){
-            var eWoonoppervlakte = descs[i+1];
-            tWoonoppervlakte = eWoonoppervlakte.innerText || eWoonoppervlakte.textContent;
-
-
-        }
-
-
     }
 
-    //replace(/[^0-9.]/g, "");
-    var nVraagprijs = tVraagprijs.replace(/[^0-9]/g, "");
-    var nWoonoppervlakte = tWoonoppervlakte.replace(/[^0-9]/g, "");
-    console.log(nVraagprijs);
-    console.log(nWoonoppervlakte);
-    nVierkanteMeterPrijs = nVraagprijs/nWoonoppervlakte;
-    alert(nVierkanteMeterPrijs);
+    if (bDetail){
+        var objects = document.querySelectorAll(".object-kenmerken-list > *, .object-kenmerken-group-list > dl > *");
+        var tWoonoppervlakte;
+        var tVraagprijs;
+        var nVierkanteMeterPrijs;
 
+        for (var i=0; i < objects.length; ++i){
+            var object = objects[i];
+            var text = object.innerText || object.textContent;
+            console.log(text);
+
+            if (text == 'Vraagprijs'){
+                var eVraagprijs = objects[i+1];
+                tVraagprijs = eVraagprijs.innerText || eVraagprijs.textContent;
+
+            }
+
+            /* Gebruiksoppervlakten*/
+            if (text  == 'Woonoppervlakte'){
+                var eWoonoppervlakte = objects[i+1];
+                tWoonoppervlakte = eWoonoppervlakte.innerText || eWoonoppervlakte.textContent;
+
+
+            }
+
+
+        }
+
+        //replace(/[^0-9.]/g, "");
+        var nVraagprijs = tVraagprijs.replace(/[^0-9]/g, "");
+        var nWoonoppervlakte = tWoonoppervlakte.replace(/[^0-9]/g, "");
+       
+
+        var nVierkanteMeterPrijs = nVraagprijs/nWoonoppervlakte;
+        var eVierkanteMeterPrijs = createEVierkanteMeterPrijs(nVierkanteMeterPrijs);
+        document.querySelector('.object-header-pricing').appendChild(eVierkanteMeterPrijs);
+
+    }
+    function createEVierkanteMeterPrijs(nVierkanteMeterPrijs){
+        var eVierkanteMeterPrijs = document.createElement("span");
+        var tVierkanteMeterPrijs = document.createTextNode(nVierkanteMeterPrijs + ' eur/m2');
+        eVierkanteMeterPrijs.appendChild(tVierkanteMeterPrijs);
+        return eVierkanteMeterPrijs;
+    }
 
 })();
